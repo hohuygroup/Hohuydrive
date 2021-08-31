14cff61fa557c6cacaef0112c4582af41c3fb1ef
hohuygroup <85934897+hohuygroup@users.noreply.github.com>
2021-08-31 19:02:26 +0700

    Update Shut_down.py

--- Shut_down.py
+++ Shut_down.py
@@ -7,6 +7,14 @@ window.geometry("1600x900")
 window.resizable(0, 0)
 window.iconbitmap("Image\logo.ico")
 
-Image = ImageTk.PhotoImage(Image.open(Backround))
+Image = ImageTk.PhotoImage(Image.open("Image/Backround/backrounds.png"))
+Backround = Label(window, image=Image)
+Backround.place(x=-5, y=0)
+
+a = Label(window, text="Đang tắt",  font=("Arial Bold", 20), bg="#000000", fg="#ffffff")
+a.place(x=730, y=550)
+
+b = Label(window, text="Tắt sau 5 giây",  font=("Arial Bold", 20), bg="#000000", fg="#ffffff")
+b.place(x=690, y=410)
 
 window.mainloop()
